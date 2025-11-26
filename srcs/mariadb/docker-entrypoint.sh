aafe94c7322bc5245977fb36d58171e79152efb1
lguietVM <lisetuieg@gmail.com>
2025-11-26 11:36:13 +0100

    adding env var for stdard user password

--- srcs/mariadb/docker-entrypoint.sh
+++ srcs/mariadb/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 
 echo "Hello, I am Lise. I certify that this image is custom."
 
-# met les vairables d'env dans l'env a partir du template puis le suppr
+# put env variables in the env from template
 if [ -f /docker-entrypoint-initdb.d/init.sql.template ]; then
   envsubst < /docker-entrypoint-initdb.d/init.sql.template > /etc/mysql/init.sql
   rm /docker-entrypoint-initdb.d/init.sql.template
--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -7,7 +7,6 @@ echo "Hello, I am Lise. I certify that this image is custom.\n"
 cd /var/www/html
 
 
-# Vérifier si WordPress est déjà installé
 if [ ! -f wp-config.php ]; then
     echo "Installing WordPress..."
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -19,8 +18,7 @@ if [ ! -f wp-config.php ]; then
     ./wp-cli.phar core install --url=lguiet.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
 
 
-    # Création de l'utilisateur lisux avec le même mot de passe que l'admin mais sans droits administrateur
-    ./wp-cli.phar user create lisux lisux@lol.com --user_pass=$WP_PASS --role=author --allow-root
+    ./wp-cli.phar user create lisux lisux@lol.com --user_pass=$WP_USER_PASS --role=author --allow-root
 else
     echo "WordPress is already installed, skipping installation."
 fi
